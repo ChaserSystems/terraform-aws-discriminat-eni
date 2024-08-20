a191cf756e103974e6c2fd9213fd0d30f24987e8
dhruv <dhruv@chasersystems.com>
2024-08-20 10:53:38 +0100

    data resource update to v2.8

--- discriminat.tf
+++ discriminat.tf
@@ -88,7 +88,7 @@ data "aws_ami" "discriminat" {
 
   filter {
     name   = "name"
-    values = var.ami_name == null ? ["DiscrimiNAT-2.7.*"] : [var.ami_name]
+    values = var.ami_name == null ? ["DiscrimiNAT-2.8.*"] : [var.ami_name]
   }
 }
 
